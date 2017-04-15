4cc5723d078d8449514bd859bbb2a68bdb3be3a0
Braden Katzman <bradenkatzman@Bradens-MacBook-Pro.local>
2017-04-15 14:12:35 -0400

    map expansion and contraction

---
+++ Assets/Scripts/MapResize_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapResize_Script : MonoBehaviour {
+
+	public Button ResizeMap_Button;
+	private RectTransform rTrans_Panel; // reference to panel transform for efficiency
+	private Vector3 initialScale_Panel; // the initial size of the minimap panel
+	private float initialYPos;
+
+	private bool expanded;
+
+	void Start () {
+		this.rTrans_Panel = transform.GetComponent<RectTransform> ();
+		this.initialScale_Panel = rTrans_Panel.localScale;
+		this.initialYPos = rTrans_Panel.anchoredPosition.y;
+		Debug.Log (initialYPos);
+
+		ResizeMap_Button.onClick.AddListener (ResizeMap);
+
+		this.expanded = false;
+	}
+
+	void ResizeMap() {
+		if (expanded) {
+			rTrans_Panel.localScale = new Vector3 (initialScale_Panel.x, initialScale_Panel.y, initialScale_Panel.z);
+			rTrans_Panel.anchoredPosition = new Vector3 (0, initialYPos, 0);
+			expanded = false;
+		} else {
+			rTrans_Panel.localScale = new Vector3(initialScale_Panel.x * 2.0f, initialScale_Panel.y * 2.0f, initialScale_Panel.z * 2.0f);
+			rTrans_Panel.anchoredPosition = new Vector3 (0, -430.0f, 0);
+			expanded = true;
+		}
+	}
+}
