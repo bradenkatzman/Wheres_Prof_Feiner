913d46914c8d2126cd4b7639c6f7b17162cbb992
Braden Katzman <bradenkatzman@gmail.com>
2017-04-25 19:25:35 -0400

    changes before kivis experiment

--- Assets/Scripts/Checkpoint5_Scripts/PaperAirplane_Flying_Script.cs
+++ Assets/Scripts/Checkpoint5_Scripts/PaperAirplane_Flying_Script.cs
@@ -10,13 +10,37 @@ public class PaperAirplane_Flying_Script : MonoBehaviour {
 	public GameObject BottomLeft;
 	public GameObject BottomRight;
 
-	// Use this for initialization
-	void Start () {
+	public float speed;
 
+	private int curr_direction;
+	private int towardBottomRight = 1;
+	private int towardTopRight = 2;
+	private int towardBottomLeft = 3;
+	private int towardTopLeft = 4;
+
+	void Start () {
+		curr_direction = towardBottomRight;
 	}
 
-	// Update is called once per frame
 	void Update () {
+		// check which direction
+		if (curr_direction == towardBottomRight) {
+			Debug.Log ("Moving");
+			// check if the plane has reached the destination
+			if (transform.position == BottomRight.transform.position) {
+				// change the direction
+				curr_direction = towardTopRight;
+			} else {
+				// move toward the target
+				float step = speed * Time.deltaTime;
+				transform.position = Vector3.MoveTowards (transform.position, BottomRight.transform.position, step);
+			}
+		} else if (curr_direction == towardTopRight) {
 
+		} else if (curr_direction == towardBottomLeft) {
+
+		} else if (curr_direction == towardTopLeft) {
+
+		}
 	}
 }
