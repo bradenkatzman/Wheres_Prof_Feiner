ec5c6f7c83ccd15adecf08d8442f1c4f96888fe2
Braden Katzman <bradenkatzman@gmail.com>
2017-04-25 18:57:50 -0400

    checkpoint 4 just missing dialogue, needs updated image targets

---
+++ Assets/Scripts/Checkpoint4_Scripts/Goblin_Money_Collider_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_Money_Collider_Script : MonoBehaviour {
+
+	private string Money_GameObject_Name;
+	private bool goblinPaid;
+
+	// Use this for initialization
+	void Start () {
+		goblinPaid = false;
+		Money_GameObject_Name = "money";
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.name.ToLower() == Money_GameObject_Name.ToLower()) {
+			Debug.Log ("Goblin paid");
+			goblinPaid = true;
+		}
+	}
+
+	public bool isGoblinPaid() {
+		return this.goblinPaid;
+	}
+}
---
+++ Assets/Scripts/Checkpoint5_Scripts/PaperAirplane_Flying_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperAirplane_Flying_Script : MonoBehaviour {
+
+	// used to translate toward these points to simulate flying
+	public GameObject TopLeft;
+	public GameObject TopRight;
+	public GameObject BottomLeft;
+	public GameObject BottomRight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
