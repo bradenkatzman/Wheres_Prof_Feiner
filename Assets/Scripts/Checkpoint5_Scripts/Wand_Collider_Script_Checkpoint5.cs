8916ce33cef4f3eaa871e76433d3194543f47e6a
Braden Katzman <bradenkatzman@gmail.com>
2017-04-26 10:26:00 -0400

    added letter panel to checkpoint 5 on plane selection

--- Assets/Scripts/Checkpoint5_Scripts/Wand_Collider_Script_Checkpoint5.cs
+++ Assets/Scripts/Checkpoint5_Scripts/Wand_Collider_Script_Checkpoint5.cs
@@ -1,15 +1,36 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Wand_Collider_Script_Checkpoint5 : MonoBehaviour {
 
 	public GameObject PaperAirplane;
+	public GameObject Letter_Panel;
+	public Button Letter_Dismiss_Button;
+
+	private bool planeSelected;
+
+	void Start() {
+		Letter_Dismiss_Button.onClick.AddListener (LetterDismissButtonPressed);
+	}
+
+	void LetterDismissButtonPressed() {
+		if (Letter_Panel.activeSelf) {
+			Letter_Panel.SetActive (false);
+		}
+	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.name == PaperAirplane.name) {
-			Debug.Log ("paper airplane selected");
+			if (planeSelected) {
+				Debug.Log ("Plane deselected");
+				planeSelected = false;
+			} else {
+				Debug.Log ("paper airplane selected");
+				planeSelected = true;
+				Letter_Panel.SetActive (true);
+			}
 
 			// enter the dialogue that displays the contents of the letter/email
 		}
